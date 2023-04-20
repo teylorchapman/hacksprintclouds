97501f89b6e1ee336ea9bca4d569ad78f930b909
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-04-20 16:35:32 -0500

    REplaced PlayerController
    Updated my Dev Scene

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MyCharacterController : MonoBehaviour
+{
+    [SerializeField] float gravity = 1;
+    [SerializeField] float jumpForce = 1;
+    [SerializeField] float jumpSpeed = 1;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), 0);
+        if (!controller.isGrounded)
+            jumpSpeed = jumpSpeed - (gravity * Time.deltaTime);
+        else
+            {
+                if (Input.GetButton("Jump"))
+                    jumpSpeed = jumpForce;
+                else jumpSpeed = 0;
+            };
+        input.y = jumpSpeed;
+        controller.Move(input * Time.deltaTime);
+    }
+
+    // FixedUpdate is called once per physics frame
+
+}
