68cc52eba4cd14e1829464cfb1a3aa840b97b61f
teylorchapman <teylor.chapman2@gmail.com>
2023-04-24 15:31:36 -0500

    fixing high score thingy

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour
 {
     public float highscore;
     public UnityEvent NewHighScore;
+    public Text TxScore, TxHigh; 
 
     // Start is called before the first frame update
     public void EnterHighScore(float score)
     {
+        TxScore.text = "Score:\n " + score;
         if (score > highscore)
         {
             highscore = score;
             NewHighScore.Invoke();
+            TxHigh.text = "High Score:\n " + score;
         }
     }
 }
